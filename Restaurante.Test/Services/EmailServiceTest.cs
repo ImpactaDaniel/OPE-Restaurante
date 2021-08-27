dacdc8725f54edd999ab09ac0f0e22cdb461a3b0
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-08-27 19:57:41 -0300

    Retirando dados sensiveis.

--- Restaurante.Test/Services/EmailServiceTest.cs
+++ Restaurante.Test/Services/EmailServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using Restaurante.Domain.Common.Models;
 using Restaurante.Infra.Common.Services;
 using Restaurante.Infra.Common.Settings;
+using Restaurante.Test.Services.Mock;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,15 +9,15 @@ namespace Restaurante.Test.Services
 {
     public class EmailServiceTest
     {
+        
         [Fact]
         public async Task ShouldSendEmail()
         {
             //Given a valid email
-            var settings = new SmtpEmailSettings("noreply.restaurante.mataburro@gmail.com", "TCC!mp4ct4");
-            var emailService = new SmtpEmailSenderService(settings);
-            var validEmail = "danielcity1@gmail.com";
-            var message = new Message(validEmail, "Test", "Test send email");
-
+            var settings = EmailSettingsMock.GetEmailTestSettings();
+            var message = new Message(settings.SendTo, "Test", "teste 12346");
+            var emailService = new SmtpEmailSenderService(new SmtpEmailSettings(settings.Email, settings.Password));
+            
             //When sending email
             var res = await emailService.SendAsync(message);
 
---
+++ Restaurante.Test/Services/Mock/EmailSettingsMock.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Restaurante.Test.Services.Mock
+{
+    public static class EmailSettingsMock
+    {
+        internal static EmailTestSettings GetEmailTestSettings()
+        {
+            if (File.Exists("test.settings.local.json"))
+                return new ConfigurationBuilder()
+                    .AddJsonFile("test.settings.local.json")
+                    .Build()
+                    .GetSection(nameof(EmailTestSettings))
+                    .Get<EmailTestSettings>();
+
+            return new ConfigurationBuilder()
+                    .AddJsonFile("test.settings.json")
+                    .Build()
+                    .GetSection(nameof(EmailTestSettings))
+                    .Get<EmailTestSettings>();
+        }
+    }
+    internal class EmailTestSettings
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string SendTo { get; set; }
+    }
+}
