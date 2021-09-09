a0ae541da80c5785d244e1195237bba2f9d51db1
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-09-08 21:45:35 -0300

    Adicionando suporte ao swagger.

--- Restaurante.Web/Controllers/FuncionariosController.cs
+++ Restaurante.Web/Controllers/FuncionariosController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Restaurante.Web.Controllers
 {
+    [ApiController, Route("[controller]")]
     public class FuncionariosController : APIControllerBase
     {
         public FuncionariosController(IMediator mediatr, INotifier notifier)
@@ -16,13 +17,14 @@ namespace Restaurante.Web.Controllers
         {
         }
 
+        [Route("GetAll"), HttpGet]
         public async Task<IActionResult> GetAll(CancellationToken cancellationToken = default)
         {
             var resp = await _mediator.Send(new GetAllFuncionariosRequest(), cancellationToken);
             return GetResponse(resp.Result);
         }
 
-        [HttpPost]
+        [Route("Create"), HttpPost]
         public async Task<IActionResult> CreateNew([FromBody]CreateFuncionarioRequest request, CancellationToken cancellationToken = default)
         {
             var resp = await _mediator.Send(request, cancellationToken);
