250f765ac23d18c726fe58208d9338a214062778
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-10-09 23:53:51 -0300

    Implementando testes da foto.

---
+++ Restaurante-Back-End/Restaurante.Test/Domain/Entities/PhotoTests.cs
@@ -0,0 +1,35 @@
+﻿using Restaurante.Domain.Products.Models;
+using Xunit;
+
+namespace Restaurante.Test.Domain.Entities
+{
+    public class PhotoTests
+    {
+        [Fact]
+        public void ShouldCreateNewPhotoCorrectlyWithExpectedPath()
+        {
+            //arrange
+            var expectedPath = "C:\\Teste";
+
+            //act
+            var photo = new Photo(expectedPath);
+
+            //assert
+            Assert.Equal(expectedPath, photo.Path);
+        }
+
+        [Fact]
+        public void ShouldUpdatePhotoPath()
+        {
+            //arrange
+            var expectedPath = "Test\\update";
+            var photo = new Photo("teste");
+
+            //act
+            photo.UpdatePath(expectedPath);
+
+            //assert
+            Assert.Equal(expectedPath, photo.Path);
+        }
+    }
+}
