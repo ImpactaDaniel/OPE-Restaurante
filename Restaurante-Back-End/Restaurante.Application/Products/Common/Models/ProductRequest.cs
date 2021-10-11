9ba6502671f27254c1b495ddbe0980c6e6911768
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-10-11 17:01:36 -0300

    Definindo product request

---
+++ Restaurante-Back-End/Restaurante.Application/Products/Common/Models/ProductRequest.cs
@@ -0,0 +1,27 @@
+﻿using Restaurante.Application.Common;
+
+namespace Restaurante.Application.Products.Common.Models
+{
+    public class ProductRequest<TRequest> : EntityRequest<int>
+        where TRequest : EntityRequest<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int QuantityStock { get; set; }
+        public PhotoRequest Photo { get; set; }
+        public ProductCategoryRequest Category { get; set; }
+        public string Accompaniments { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
+    }
+
+    public class PhotoRequest
+    {
+        public string PhotoPath { get; set; }
+    }
+
+    public class ProductCategoryRequest
+    {
+        public string Name { get; set; }
+    }
+}
