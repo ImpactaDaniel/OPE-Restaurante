fbdfffae9736a871cb2013800c624ef980ee66b0
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-10-11 17:11:22 -0300

    Definindo teste para criação de produto.

---
+++ Restaurante-Back-End/Restaurante.Test/Services/ProductServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using NSubstitute;
+using Restaurante.Domain.Products.Models;
+using Restaurante.Domain.Products.Repositories.Interfaces;
+using Restaurante.Domain.Products.Services.Interfaces;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Restaurante.Test.Services
+{
+    public class ProductServiceTests
+    {
+        private readonly IProductService _service;
+        private readonly IProductDomainRepository _repository;
+
+        public ProductServiceTests()
+        {
+            _repository = Substitute.For<IProductDomainRepository>();
+        }
+
+        [Fact]
+        public async Task ShouldCreateNewProduct()
+        {
+            //arrange
+            _repository.Save(Arg.Any<Product>()).ReturnsForAnyArgs(true);
+            var product = new Product("test", "test", 10m, true);
+
+            //act
+            await _service.CreateProduct(product);
+
+            //assert
+            await _repository.ReceivedWithAnyArgs().Save(Arg.Any<Product>());
+        }
+    }
+}
