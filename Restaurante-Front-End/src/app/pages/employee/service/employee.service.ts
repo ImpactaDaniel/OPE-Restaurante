90198f9938ee5324aaac7708d0b4c742f9816a73
ImpactaDenis <denis.tiago@aluno.faculdadeimpacta.com.br>
2021-11-09 21:31:12 -0300

    Ajustes de pesquisa em tela de funcionarios

--- Restaurante-Front-End/src/app/pages/employee/service/employee.service.ts
+++ Restaurante-Front-End/src/app/pages/employee/service/employee.service.ts
@@ -14,13 +14,25 @@ export class EmployeeService {
 
   constructor(@Inject('BASE_URL') private url: string, private httpClient: HttpClient) { }
 
-  createEmployee(employee: Employee): Observable<APIResponse<boolean>> {
+  public createEmployee(employee: Employee): Observable<APIResponse<boolean>> {
     return this.httpClient.post<APIResponse<boolean>>(this.url + 'Employees/Create', employee)
   }
 
-  public getAllInvoices(): any {
+  public getEmployeeById(invoiceId: number): any {
     return this.httpClient.get<any>(
-      this.url + this.urlInvoices + "/GetAll"
+      this.url + this.urlInvoices + "/Get/" + invoiceId
+    );
+  }
+
+  public getAllEmployees(page: number, limit: number): any {
+    return this.httpClient.get<any>(
+      this.url + this.urlInvoices + "/GetAll?page=" + page + "&limit=" + limit
+      );
+  }
+
+  public searchEmployees(field: string, value: string, page: number, limit: number): any {
+    return this.httpClient.get<APIResponse<any>>(
+      this.url + this.urlInvoices + "/Search?&field=" + field + "&value=" + value + "&page=" + page + "&limit=" + limit
       );
   }
 }
