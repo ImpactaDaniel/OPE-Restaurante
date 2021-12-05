c280182540f001d6c4723a4fa877acf95247e4a2
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-12-05 11:14:05 -0300

    Definindo entidades.

---
+++ Restaurante-Back-End/Restaurante.Domain/Baskets/Models/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿using Restaurante.Domain.Common.Models;
+using Restaurante.Domain.Products.Models;
+
+namespace Restaurante.Domain.Baskets.Models
+{
+    public class BasketItem : Entity<int>
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public string Obs { get; set; }
+    }
+}
